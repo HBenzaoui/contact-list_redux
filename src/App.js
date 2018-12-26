3cda16c5c3bda65a932be6491f88275aa1cd08e9
Hamza Benzaoui <hamzapdd@gmail.com>
2018-12-26 15:07:59 +0100

    install bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import Header from './component/Header';
+import Contact from './component/Contact';
 
 class App extends Component {
   render() {
     return (
       <div>
         <Header branding="Contact Manager" />
+        <Contact
+          name="Hamza"
+          email="Hamzapdd@gmail.com"
+          phone="0661 45 81 97"
+        />
       </div>
     );
   }
--- src/component/Contact.js
+++ src/component/Contact.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const Contact = () => {
-  return <div />;
+const Contact = props => {
+  return (
+    <div>
+      <h3>Name:{props.name}</h3>
+      <p>Email: {props.email}</p>
+      <p>Phone: {props.phone}</p>
+    </div>
+  );
 };
 
 export default Contact;

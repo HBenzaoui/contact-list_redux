6ffd479293be0a399618f74cde2fb7ae6b233708
Hamza Benzaoui <hamzapdd@gmail.com>
2018-12-27 20:30:18 +0100

    creat About page

---
+++ src/component/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <div>
+      <h1 className="display-4">About Contact Manager</h1>
+      <p className="lead">Simple app to manage contact</p>
+      <p className="text-secndary">Version 1.0.0</p>
+    </div>
+  );
+};
+
+export default About;

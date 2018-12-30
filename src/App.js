2cf62c0bb671fa5739e049f6436353e4f0200979
Hamza Benzaoui <hamzapdd@gmail.com>
2018-12-30 19:26:32 +0100

    configut to deploy on githubpages

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends Component {
   render() {
     return (
       <Provider>
-        <Router>
+        <Router basename={process.env.PUBLIC_URL}>
           <React.Fragment>
             <Header branding="Contact Manager" />
             <div className="container">
